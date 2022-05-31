ad13a141fe66dfe2c2a6f2813ae60dc7661be2f8
lycanthropystudios <36338339+lycanthropystudios@users.noreply.github.com>
2022-05-31 16:31:18 -0500

    feat:client for mailrelay/ readme

--- mailrelay/client.py
+++ mailrelay/client.py
@@ -9,20 +9,91 @@ import requests
 class Client(object):
     def __init__(self, api_key, domain):
         self.api_key = api_key
-        self.base_url = f"https://{domain}/api/v1/"        
+        self.base_url = f"https://{domain}/api/v1/"
 
     def _compose_endpoint(self, endpoint, id=None):
         if id:
-            url = self.base_url+endpoint+"/{id}".format(id=id)
+            url = self.base_url + endpoint + "/{id}".format(id=id)
         else:
-            url = self.base_url+endpoint
+            url = self.base_url + endpoint
         return url
 
-    def get_subscribers(self)-> list:
-        """Return Subscribers data
+    def get_groups(self) -> list:
+        """Lists groups.
 
         Returns:
-            list: 
+            list: [
+            {
+                "id": 1,
+                "name": "grupo test 1",
+                "description": "",
+                "subscribers_count": 3,
+                "created_at": "2022-05-11T22:48:18.000Z",
+                "updated_at": "2022-05-11T22:48:18.000Z",
+            },
+            {
+                "id": 2,
+                "name": "grupo 2",
+                "description": "",
+                "subscribers_count": 1,
+                "created_at": "2022-05-20T15:38:13.000Z",
+                "updated_at": "2022-05-20T15:38:13.000Z",
+            },
+        ]
+        """        
+        url = self._compose_endpoint("groups")
+        return self._get(url=url)
+    
+    def get_campaigns(self) -> list:
+        """Lists campaigns.
+
+        Returns:
+            [
+                {
+                    "id": 0,
+                    "subject": "string",
+                    "sender_id": 0,
+                    "campaign_folder_id": 0,
+                    "target": "groups",
+                    "segment_id": 0,
+                    "group_ids": "string",
+                    "preview_text": "string",
+                    "html": "string",
+                    "editor_type": "html",
+                    "url_token": false,
+                    "analytics_utm_campaign": "string",
+                    "use_premailer": false
+                }
+            ]
+        """
+        url = self._compose_endpoint("campaigns")
+        return self._get(url=url)
+
+    def send_campaigns(self, id, data:dict) -> dict:
+        """This method is used to send a campaign to groups or a specific segment. It will return some attributes, including the ID of the sent campaign which can be used to check the progress and results of the campaign
+
+        Args:
+            id (_type_): _description_
+            data (dict): {
+                            "target": "groups",
+                            "group_ids": [
+                                0
+                            ],
+                            "segment_id": 0,
+                            "scheduled_at": "1970-01-01T00:00:00.000Z",
+                            "callback_url": "string"
+                        }
+        Returns:
+            dict: _description_
+        """
+        url = self.base_url + "campaigns/{id}/send_all".format(id=id)
+        return self._post(url=url, json=data)
+
+    def get_subscribers(self) -> list:
+        """Lists all subscribers.
+
+        Returns:
+            list:
             [
                 {
                     "id": 1,
@@ -54,25 +125,125 @@ class Client(object):
                     "custom_fields": {},
                 },
             ]
-        """        
+        """
         url = self._compose_endpoint("subscribers")
         return self._get(url=url)
 
-    def get_subscriber_by_id(self, id:int)-> dict:
-        """Get subscriber by id
+    def get_subscriber_by_id(self, id: int) -> dict:
+        """Get a subscriber by id
 
         Args:
             id (int): _description_
 
         Returns:
-            dict: _description_
+            dict: {
+                "id": 0,
+                "email": "user@example.com",
+                "name": "string",
+                "score": 0,
+                "status": "string",
+                "subscribed_at": "1970-01-01T00:00:00.000Z",
+                "subscribed_with_acceptance": false,
+                "subscribe_ip": "198.51.100.42",
+                "unsubscribed": false,
+                "unsubscribed_at": "1970-01-01T00:00:00.000Z",
+                "unsubscribe_ip": "198.51.100.42",
+                "unsubscribe_sent_email_id": 0,
+                "address": "string",
+                "city": "string",
+                "state": "string",
+                "country": "string",
+                "birthday": "1970-01-01",
+                "website": "string",
+                "time_zone": "Africa/Abidjan",
+                "locale": "en",
+                "bounced": false,
+                "reported_spam": false,
+                "local_ban": false,
+                "global_ban": false,
+                "created_at": "1970-01-01T00:00:00.000Z",
+                "updated_at": "1970-01-01T00:00:00.000Z",
+                "groups": [
+                    {
+                    "group_id": 0,
+                    "assigned_at": "1970-01-01T00:00:00.000Z"
+                    }
+                ],
+                "custom_fields": {}
+                }
         """
-        url = self._compose_endpoint(endpoint="subscribers", id=id)    
+        url = self._compose_endpoint(endpoint="subscribers", id=id)
         return self._get(url=url)
 
-    def create_subscriber(self, data)-> response:
+    def create_subscriber(self, data:dict) -> dict:
+        """Add a new subscriber
+
+        Note that you should provide the default subscriber status.
+
+        active: use this status if the subscriber has already gone through your opt in process and confirmed that he wants to receive your newsletters.
+        inactive: if you use this option, you must use the resend_confirmation_email method to send the confirmation email to the subscriber. Only after clicking at link in the confirmation email, he will be changed to active status and will receive your newsletters.
+
+        Args:
+            data (dict): {
+                "status": "active",
+                "email": "user@example.com",
+                "name": "string",
+                "address": "string",
+                "city": "string",
+                "state": "string",
+                "country": "string",
+                "birthday": "1970-01-01",
+                "website": "string",
+                "locale": "en",
+                "time_zone": "Africa/Abidjan",
+                "group_ids": [
+                    0
+                ]
+            }
+
+        Returns:
+            dict: {
+                    "id": 0,
+                    "email": "user@example.com",
+                    "name": "string",
+                    "score": 0,
+                    "status": "string",
+                    "subscribed_at": "1970-01-01T00:00:00.000Z",
+                    "subscribed_with_acceptance": false,
+                    "subscribe_ip": "198.51.100.42",
+                    "unsubscribed": false,
+                    "unsubscribed_at": "1970-01-01T00:00:00.000Z",
+                    "unsubscribe_ip": "198.51.100.42",
+                    "unsubscribe_sent_email_id": 0,
+                    "address": "string",
+                    "city": "string",
+                    "state": "string",
+                    "country": "string",
+                    "birthday": "1970-01-01",
+                    "website": "string",
+                    "time_zone": "Africa/Abidjan",
+                    "locale": "en",
+                    "bounced": false,
+                    "reported_spam": false,
+                    "local_ban": false,
+                    "global_ban": false,
+                    "created_at": "1970-01-01T00:00:00.000Z",
+                    "updated_at": "1970-01-01T00:00:00.000Z",
+                    "groups": [
+                        {
+                        "group_id": 0,
+                        "assigned_at": "1970-01-01T00:00:00.000Z"
+                        }
+                    ],
+                    "custom_fields": {}
+                }
+        """
         url = self._compose_endpoint("subscribers")
         return self._post(url=url, json=data)
+
+    def delete_subscriber(self, id=id) -> response:
+        url = self._compose_endpoint("subscribers", id=id)
+        return self._delete(url=url)
 
     def _get(self, url, **kwargs):
         return self._request("GET", url, **kwargs)
@@ -160,30 +331,3 @@ class Client(object):
                 raise exceptions.ServiceUnavailable(r)
             raise exceptions.UnknownError(r)
 
-
-if __name__ == "__main__":
-    API_KEY = ""
-    DOMAIN = ""
-    client = Client(API_KEY, DOMAIN)
-    # campaigns = client.get_sent_campaigns(sender_id_eq=2)
-    # campaign = client.get_campaigns()
-    d = {
-        "status": "active",
-        "email": "johanntest@example.com",
-        "name": "johann",
-        "address": "calle",
-        "city": "buga",
-        "state": "valle",
-        "country": "CO",
-        "birthday": "1970-01-01",
-        
-        "locale": "en",
-        "time_zone": "",
-        "group_ids": [1],
-    }
-    
-    print(d)
-    campaign = client.create_subscriber(d)
-    #campaign = client.get_subscribers()
-    #campaign = client.get_subscriber_by_id(id=1)
-    print(campaign)
